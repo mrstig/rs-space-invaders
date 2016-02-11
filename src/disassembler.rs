f973ddcd26f3945dd4d27e31d4dbb05a226d3830
Sondre Hisdal <slaich87@gmail.com>
2016-02-11 18:27:42 +0100

    update dissasembler

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -5,14 +5,24 @@ pub fn disassemble(code_buffer: &Vec<u8>, pc: usize) -> usize {
 
     match code {
         0x00 => {print!("NOP");},
-        0x01 => {print!("LXI"); opbytes = 3},
-        0x02 => {print!("STAX   ");},
-        0x03 => {print!("INX    ");},
-        0x04 => {print!("INR    ");},
-        0x05 => {print!("DCR    ");},
-        0x06 => {print!("MVI    "); opbytes = 2},
+        0x01 => {print!("LXI B, {:x}{:x}", code_buffer[pc +2], code_buffer[pc +1]); opbytes = 3},
+        0x02 => {print!("STAX B");},
+        0x03 => {print!("INX B");},
+        0x04 => {print!("INR B");},
+        0x05 => {print!("DCR B");},
+        0x06 => {print!("MVI B, {:x}", code_buffer[pc + 1]); opbytes = 2},
         0x07 => {print!("RLC");},
         0x08 => {print!("NOP");},
+        0x09 => {print!("DAD B");},
+        0x0a => {print!("LDAX B");},
+        0x0b => {print!("DCX B");},
+        0x0c => {print!("INR C");},
+        0x0d => {print!("DCR C");},
+        0x0e => {print!("MVI C"); opbytes = 2},
+        0x0f => {print!("RRC");},
+        0x10 => {print!("NOP");},
+        0x31 => {print!("LXI SP, {:02X}{:02X}", code_buffer[pc + 2], code_buffer[pc + 1]);  opbytes = 3;},
+        0xc3 => {print!("JMP adr, {:02X}{:02X}", code_buffer[pc + 2], code_buffer[pc + 1]); opbytes = 3},
         _ => print!("Something else")
     }
 
--- src/romloader.rs
+++ src/romloader.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn load() -> Vec<u8> {
-    let mut f = File::open("D:/Development/rs-space-invaders/res/invaders.rom").unwrap();
+    // let mut f = File::open("D:/Development/rs-space-invaders/res/invaders.rom").unwrap();
+    let mut f = File::open("G:/development/private/rs-space-invaders/res/invaders.rom").unwrap();
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer).unwrap();
     return buffer;
