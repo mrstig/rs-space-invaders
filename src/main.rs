95e627e702b931eb33f44994b6a5eb6eff1fc54d
mrstig <mrstig@gmail.com>
2016-02-11 12:15:07 +0100

    Match rename of disassembler.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod romloader;
-mod disassemler;
+mod disassembler;
 
 fn main() {
     let rom = romloader::load();
     let mut pc:usize = 0;
     while pc < rom.len(){
-        pc += disassemler::disassemble(&rom, pc);
+        pc += disassembler::disassemble(&rom, pc);
     }
     println!("{:?}", &rom.len());
 }
