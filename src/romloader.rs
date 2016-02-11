30d75bc0a1f8e22a29fc18bb7f21243eec8e5c5d
Sondre Hisdal <slaich87@gmail.com>
2016-02-11 18:29:48 +0100

    fix rom path

--- src/romloader.rs
+++ src/romloader.rs
@@ -2,8 +2,7 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn load() -> Vec<u8> {
-    // let mut f = File::open("D:/Development/rs-space-invaders/res/invaders.rom").unwrap();
-    let mut f = File::open("G:/development/private/rs-space-invaders/res/invaders.rom").unwrap();
+    let mut f = File::open("../res/invaders.rom").unwrap();
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer).unwrap();
     return buffer;
